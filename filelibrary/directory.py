a2fe360134e1a3aed4a700f55218e808927fd440
alexr <alexreynen@gmail.com>
2019-07-04 13:15:36 -0700

    Add tests and conditions for a directory that isn't a directory

--- filelibrary/directory.py
+++ filelibrary/directory.py
@@ -8,13 +8,24 @@ import os
 class Directory:
 
     def __init__(self, dir):
-        self.name = str.split(dir, '/')[-1]
-        self.path = dir
+        if self.is_dir(dir):
+            self.name = str.split(dir, '/')[-1]
+            self.path = dir
+        else:
+            self.name = None
+            self.path = None
 
     def __str__(self):
         return f'Located at path: {self.path}\n' + f'It is named: {self.name}'
+
+    # Returns True if it is a directory, False if it is not
+    def is_dir(self, dir):
+        return True if os.path.isdir(dir) else False
 
 
 if __name__ == '__main__':
     bb = Directory(os.path.dirname(__file__) + '/../tests/test')
     print(bb)
+
+    aa = Directory(os.path.dirname(__file__) + '/../tests/test.test.txt')
+    print(aa)
--- tests/test_directory.py
+++ tests/test_directory.py
@@ -4,14 +4,18 @@ import os
 from filelibrary import Directory
 
 TEST_DIR_PATH = os.path.dirname(__file__) + '/../tests/test'
+TEST_TXT_FILE = TEST_DIR_PATH + '/test.txt'
 
 test_dir = Directory(TEST_DIR_PATH)
+test_file = Directory(TEST_TXT_FILE)
 
 
 class DirectoryTest(unittest.TestCase):
 
-    def test_is_directory(self):
-        self.assertTrue(TEST_DIR_PATH)
-
-    def test_class_variables(self):
+    def test_class_variables_real_dir(self):
         self.assertEqual(test_dir.path, TEST_DIR_PATH)
+        self.assertEqual(test_dir.name, str.split(TEST_DIR_PATH, '/')[-1])
+
+    def test_class_variables_file(self):
+        self.assertIsNone(test_file.path)
+        self.assertIsNone(test_file.name)
