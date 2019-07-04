d34d0b29460dc5662080bd876707d2204763ab55
alexr <alexreynen@gmail.com>
2019-07-04 16:01:23 -0700

    Begin obtaining tags for mp3 files and flac files

--- filelibrary/audio_file.py
+++ filelibrary/audio_file.py
@@ -3,6 +3,8 @@
 # Author: Alex Reynen
 
 import os
+from mutagen.flac import FLAC
+from mutagen.mp3 import MP3
 
 from filelibrary import File
 
@@ -13,15 +15,25 @@ class AudioFile(File):
         super().__init__(dir)
 
         if str.lower(self.extension) == 'flac':
-            print('is flac')
+            self.tag_flac()
 
         if str.lower(self.extension) == 'mp3':
-            print('is mp3')
+            self.tag_mp3()
+
+    def tag_flac(self):
+        meta_data = FLAC(self.path)
+        for key in meta_data.keys():
+            print(f'{key} = {meta_data[key][0]}')
+
+    def tag_mp3(self):
+        meta_data = MP3(self.path)
+        for key in meta_data.keys():
+            print(f'{key} = {meta_data[key]}')
 
 
 if __name__ == '__main__':
     flac_file = AudioFile(os.path.dirname(__file__) + '/../tests/test/audio_files/example.flac')
-    print (flac_file)
+    print(flac_file)
 
     mp3_file = AudioFile(os.path.dirname(__file__) + '/../tests/test/audio_files/example.mp3')
     print(mp3_file)
