c6a62e90e3b633cd50469182989f778c77da9a41
alexr <alexreynen@gmail.com>
2019-07-04 17:39:17 -0700

    Properly work with the audio files to grab the tags properly

--- filelibrary/audio_file.py
+++ filelibrary/audio_file.py
@@ -13,6 +13,8 @@ class AudioFile(File):
 
     def __init__(self, dir):
         super().__init__(dir)
+        self.disc_number = self.date = self.album = self.genre = self.track_number = self.artist = self.album_artist =\
+            self.title = self.composer = None
 
         if str.lower(self.extension) == 'flac':
             self.tag_flac()
@@ -20,15 +22,40 @@ class AudioFile(File):
         if str.lower(self.extension) == 'mp3':
             self.tag_mp3()
 
+    def __str__(self):
+        return super().__str__() +\
+               f'Disc Number = {self.disc_number}\n' +\
+               f'Album = {self.album}\n' +\
+               f'Genre = {self.genre}\n' +\
+               f'Track Number = {self.track_number}\n' +\
+               f'Artist = {self.artist}\n' +\
+               f'Album Artist = {self.album_artist}\n' +\
+               f'Title = {self.title}\n' +\
+               f'Composer = {self.composer}\n'
+
     def tag_flac(self):
         meta_data = FLAC(self.path)
-        for key in meta_data.keys():
-            print(f'{key} = {meta_data[key][0]}')
+        self.disc_number = meta_data['discnumber'][0]
+        self.date = meta_data['date'][0]
+        self.album = meta_data['album'][0]
+        self.genre = meta_data['genre'][0]
+        self.track_number = meta_data['tracknumber'][0]
+        self.artist = meta_data['artist'][0]
+        self.album_artist = meta_data['albumartist'][0]
+        self.title = meta_data['title'][0]
+        self.composer = meta_data['composer'][0]
 
     def tag_mp3(self):
         meta_data = MP3(self.path)
-        for key in meta_data.keys():
-            print(f'{key} = {meta_data[key]}')
+        self.disc_number = meta_data['TPOS']
+        self.date = meta_data['TDRC']
+        self.album = meta_data['TALB']
+        self.genre = meta_data['TCON']
+        self.track_number = meta_data['TRCK']
+        self.artist = meta_data['TPE1']
+        self.album_artist = meta_data['TPE2']
+        self.title = meta_data['TIT2']
+        self.composer = meta_data['TCOM']
 
 
 if __name__ == '__main__':
